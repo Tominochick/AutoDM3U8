42c3e2b36d16fd1ecd7ae3b69e89408ee735176b
Pan_li <63991331+Tominochick@users.noreply.github.com>
2023-02-14 22:54:33 +0800

    上传项目

--- AutoDM3U8.py
+++ AutoDM3U8.py
@@ -16,7 +16,7 @@ Waiting_dir = os.path
 Downloading_dir = os.path
 Success_dir = os.path
 Failure_dir = os.path
-Result_dir = "RESULT"
+Result_dir = os.path
 
 
 def mkdirsIfNotExist(*path: os.path):
@@ -33,7 +33,8 @@ with open("AutoDM3U8.pid", mode="w") as f:
     Downloading_dir = os.path.join(Run_dir, "TASK-DOWNLOADING")
     Success_dir = os.path.join(Run_dir, "TASK-SUCCESS")
     Failure_dir = os.path.join(Run_dir, "TASK-FAILURE")
-    mkdirsIfNotExist(Waiting_dir, Downloading_dir, Success_dir, Failure_dir)
+    Result_dir = os.path.join(Run_dir, "RESULT")
+    mkdirsIfNotExist(Waiting_dir, Downloading_dir, Success_dir, Failure_dir, Result_dir)
 if sys_type == "Windows":
     N_m3u8DL_RE_path = "N_m3u8DL-RE_Beta_win-x64\\N_m3u8DL-RE.exe "
     str_encoding = "gbk"
@@ -52,6 +53,11 @@ def onSubmit(t_filename: str):
     shutil.move(t_file, Downloading_dir)
 
 
+def onSubmitFailure(t_filename: str):
+    t_file = os.path.join(Waiting_dir, t_filename)
+    shutil.move(t_file, Failure_dir)
+
+
 def onFailure(t_filename: str):
     t_file = os.path.join(Downloading_dir, t_filename)
     shutil.move(t_file, Failure_dir)
@@ -68,7 +74,7 @@ def createTask(t_args: str, t_filename: str):
     if t_args.find("N_m3u8DL-RE") == -1:
         run_args = N_m3u8DL_RE_path + t_args
     if t_args.find("--save-dir") == -1:
-        run_args += " --save-dir " + Result_dir
+        run_args += " --save-dir " + Result_dir.__str__()
     if t_args.find("--save-name") == -1:
         run_args += " --save-name " + t_filename + "###" + time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime())
 
@@ -78,7 +84,7 @@ def createTask(t_args: str, t_filename: str):
         try:
             while True:
                 buff = proc.stdout.readline()
-                print("TASK process:" + proc.pid.__str__() + "stdout=" + buff, end="")
+                print("TASK process:" + proc.pid.__str__() + " " + buff, end="")
                 if buff.find("ERROR") > -1:
                     print("TASK process:" + proc.pid.__str__() + " 失败")
                     onFailure(t_filename)
@@ -106,10 +112,13 @@ if __name__ == '__main__':
         for file in files:
             if file.endswith(cmd_suffix):
                 task_file = os.path.join(Waiting_dir, file)
-                with open(task_file, mode="r", encoding="utf-8") as f:
-                    args = ""
-                    for line in f.readlines():
-                        args += line
-                createTask(args, file)
+                try:
+                    with open(task_file, mode="r", encoding="utf-8") as f:
+                        args = f.read()
+                    createTask(args, file)
+                except Exception as e:
+                    print(e)
+                    onSubmitFailure(file)
             else:
                 print("任务文件必须以" + cmd_suffix + "结尾")
+                onSubmitFailure(file)
